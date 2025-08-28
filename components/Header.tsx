f77d2d7469949f52192783e81d673d20c81775fe
rururu5581 <sakakurarumiko@gmail.com>
2025-08-28 15:05:19 +0900

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,17 @@
-
 import React from 'react';
 
 const Header: React.FC = () => {
   return (
     <header className="bg-white shadow-sm">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div className="py-4 border-b-2 border-[#FF0000]">
-          <h1 className="text-2xl font-bold text-black">
-            morich Recommender Pro
-          </h1>
+        <div className="py-4 border-b-2 border-[#FF0000] flex items-center justify-between">
+          <div className="flex items-center">
+            {/* publicフォルダのSVGを参照 */}
+            <img src="/morich2.svg" alt="morich logo" className="h-8 mr-3" />
+            <h1 className="text-3xl font-extrabold text-[#FF0000]">
+              morich Recommender Pro
+            </h1>
+          </div>
         </div>
       </div>
     </header>
