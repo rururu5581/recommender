817470f665d16e3e1680322b0a798865f2eca6e1
rururu5581 <sakakurarumiko@gmail.com>
2025-08-31 12:38:23 +0900

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,8 +6,8 @@ const Header: React.FC = () => {
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="py-4 border-b-2 border-[#FF0000] flex items-center justify-between">
           <div className="flex items-center">
-            {/* publicフォルダのSVGを参照 */}
-            <img src="/morich2.svg" alt="morich logo" className="h-8 mr-3" />
+            {/* publicフォルダのPNG画像を参照 */}
+            <img src="/morich2.png" alt="morich logo" className="h-8 mr-3" />
             <h1 className="text-3xl font-extrabold text-[#FF0000]">
               morich Recommender Pro
             </h1>
